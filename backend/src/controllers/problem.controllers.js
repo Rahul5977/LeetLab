950c7c6ea73c3c8b882a7d3e255a82a183425fa2
Rahul Raj <rahulraj@Rahuls-MacBook-Air.local>
2025-07-05 01:02:36 +0530

    add: createProblem-controller

--- backend/src/controllers/problem.controllers.js
+++ backend/src/controllers/problem.controllers.js
@@ -1,3 +1,67 @@
-export const createProblem =async(req,res)=>{
-    
-}
+import { db } from "../libs/db.js";
+import { getJudge0LanguageId, pollBatchResults, submitBatch } from "../libs/judge0.lib.js";
+export const createProblem = async (req, res) => {
+  const {
+    title,
+    description,
+    difficulty,
+    tags,
+    examples,
+    constraints,
+    testcases,
+    codeSnippets,
+    referenceSolutions,
+  } = req.body;
+  if (req.user.role !== "ADMIN") {
+    return res.status(403).json({
+      message: "You are not allowed to create a problem",
+    });
+  }
+  try {
+    for (const [language, solutionCode] of Object.entries(referenceSolutions)) {
+      const languageId = getJudge0LanguageId(language);
+      if (!languageId) {
+        return res.status(400).json({
+          error: `Language ${language} is not supported`,
+        });
+      }
+      const submissions = testcases.map(({ input, output }) => ({
+        souce_code: solutionCode,
+        language_id: languageId,
+        stdin: input,
+        expected_output: output,
+      }));
+      const submissionsResults = await submitBatch(submissions);
+      const tokens = submissions.map((res) => res.token);
+      const results = await pollBatchResults(tokens);
+      for (let i = 0; i < results.length; i++) {
+        const result = results[i];
+        if (result.status.id !== 3) {
+          return res.status(400).json({
+            error: `Testcase ${i + 1} failed for language ${language}`,
+          });
+        }
+      }
+      //save the problem to db
+      const newProblem = await db.problem.create({
+        data: {
+          title,
+          description,
+          difficulty,
+          tags,
+          examples,
+          constraints,
+          testcases,
+          codeSnippets,
+          referenceSolutions,
+          userId: req.user.id,
+        },
+      });
+      return res.status(201).json(newProblem);
+    }
+  } catch (error) {}
+};
+export const getAllProblem = async (req, res) => {};
+export const getAllProblemById = async (req, res) => {};
+export const updateProblem = async (req, res) => {};
+export const deleteProblem = async (req, res) => {};
--- backend/src/routes/problem.route.js
+++ backend/src/routes/problem.route.js
@@ -1,9 +1,19 @@
 import express from "express";
 import { authMiddleware, checkAdmin } from "../middleware/auth.middleware.js";
-import { createProblem } from "../controllers/problem.controllers.js";
+import {
+  createProblem,
+  deleteProblem,
+  getAllProblem,
+  getAllProblemById,
+  updateProblem,
+} from "../controllers/problem.controllers.js";
 
 const problemRoutes = express.Router();
 
-problemRoutes.post("/create-problem",authMiddleware,checkAdmin,createProblem)
+problemRoutes.post("/create-problem", authMiddleware, checkAdmin, createProblem);
+problemRoutes.get("/get-all-problems", authMiddleware, getAllProblem);
+problemRoutes.get("/get-all-problems/:id", authMiddleware, getAllProblemById);
+problemRoutes.put("/update-problem/:id", authMiddleware, checkAdmin, updateProblem);
+problemRoutes.delete("/delete-problem/:id", authMiddleware, checkAdmin, deleteProblem);
 
 export default problemRoutes;
