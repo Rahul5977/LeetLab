2603ffc3bac53c8c7df1eaf9e584acb5d07b82ea
Rahul Raj <rahul.raj9237@gmail.com>
2025-07-05 23:10:27 +0530

    getProblemById-controller done

--- backend/src/controllers/problem.controllers.js
+++ backend/src/controllers/problem.controllers.js
@@ -101,6 +101,32 @@ export const getAllProblem = async (req, res) => {
     });
   }
 };
-export const getAllProblemById = async (req, res) => {};
+export const getAllProblemById = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const problem = await db.problems.findUnique({
+      where: {
+        id: id,
+      },
+    });
+    if (!problem) {
+      return res.status(404).json({
+        error: "No problem found",
+      });
+    }
+    return res.status(200).json({
+      success: true,
+      message: "Problem fetched Successfully",
+      problem,
+    });
+
+  } catch (error) {
+    console.error("Error fetching problem by id", error);
+    res.status(403).json({
+      success: false,
+      error: "Error fetching problem by id",
+    });
+  }
+};
 export const updateProblem = async (req, res) => {};
 export const deleteProblem = async (req, res) => {};
