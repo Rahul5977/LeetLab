84edb072d604d165e2b6631ab6d938ec6b4b5cd7
Rahul Raj <rahul.raj9237@gmail.com>
2025-07-07 22:32:34 +0530

    submit-controllers-initialized

---
+++ backend/src/controllers/submission.controllers.js
@@ -0,0 +1,9 @@
+export const getAllSubmission = async (req,res)=>{
+
+}
+export const getSubmissionForProblems = async (req,res)=>{
+
+}
+export const getAllTheSubmissionForProblem = async (req,res)=>{
+
+}
--- backend/src/routes/submission.routes.js
+++ backend/src/routes/submission.routes.js
@@ -1,5 +1,18 @@
-import express from "express"
+import express from "express";
+import { authMiddleware } from "../middleware/auth.middleware.js";
+import {
+  getAllSubmission,
+  getAllTheSubmissionForProblem,
+  getSubmissionForProblems,
+} from "../controllers/submission.controllers.js";
 
-const submissionRoutes=express.Router()
+const submissionRoutes = express.Router();
+submissionRoutes.get("/get-all-submissions", authMiddleware, getAllSubmission);
+submissionRoutes.get("/get-submission/:problemId", authMiddleware, getSubmissionForProblems);
+submissionRoutes.get(
+  "/get-submission-count/:problemId",
+  authMiddleware,
+  getAllTheSubmissionForProblem
+);
 
-export default submissionRoutes
+export default submissionRoutes;
